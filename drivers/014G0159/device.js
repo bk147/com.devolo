941b84009aa957190dfe66ee733744501e7941f4
KyleAthom <kyle@athom.com>
2020-04-10 12:01:50 +0200

    Version 2.0.9 - Corrected room thermostat pairing

--- drivers/014G0159/device.js
+++ drivers/014G0159/device.js
@@ -3,10 +3,13 @@
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 // Documentation: http://heating.danfoss.com/PCMPDF/44035v01.pdf
+// https://www.devolo.de/fileadmin/Web-Content/DE/products/hc/hc-raumthermostat/documents/de/Home_Control_Raumthermostat_0916_de_online.pdf
 
 class DevoloThermostat extends ZwaveDevice {
 
 	onMeshInit() {
+		this.enableDebug();
+
 		this.registerCapability('measure_battery', 'BATTERY');
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
 		this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT');
