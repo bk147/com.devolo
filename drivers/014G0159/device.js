903e57acf8e393528aa9d37b4b39604af68d1b52
KyleAthom <kyle@athom.com>
2020-04-14 14:52:01 +0200

    Fix Room thermostat

--- drivers/014G0159/device.js
+++ drivers/014G0159/device.js
@@ -1,23 +1,35 @@
 'use strict';
 
-const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+const { ZwaveDevice } = require('homey-meshdriver');
 
 // Documentation: http://heating.danfoss.com/PCMPDF/44035v01.pdf
 // https://www.devolo.de/fileadmin/Web-Content/DE/products/hc/hc-raumthermostat/documents/de/Home_Control_Raumthermostat_0916_de_online.pdf
 
 class DevoloThermostat extends ZwaveDevice {
 
-	onMeshInit() {
-		this.enableDebug();
+  onMeshInit() {
+    this.enableDebug();
 
-		this.registerCapability('measure_battery', 'BATTERY');
-		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
-		this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT');
+    this.registerCapability('measure_battery', 'BATTERY', {
+      getOpts: {
+        getOnOnline: true,
+      },
+    });
+    this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
+      getOpts: {
+        getOnOnline: true,
+      },
+    });
+    this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT', {
+      getOpts: {
+        getOnOnline: true,
+      },
+    });
 
-		this.registerReportListener('CENTRAL_SCENE', 'CENTRAL_SCENE_NOTIFICATION', (report) => {
-			if (report && report['Scene Number'] === 1) this.getDriver().buttonTrigger.trigger(this, null, null);
-		});
-	}
+    this.registerReportListener('CENTRAL_SCENE', 'CENTRAL_SCENE_NOTIFICATION', report => {
+      if (report && report['Scene Number'] === 1) this.getDriver().buttonTrigger.trigger(this, null, null);
+    });
+  }
 
 }
 
