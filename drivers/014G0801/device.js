de98449bcf985b705ea576e389fc28b2422241b6
KyleAthom <kyle@athom.com>
2020-04-15 11:05:11 +0200

    Version 2.0.10 - Fix thermostats

--- drivers/014G0801/device.js
+++ drivers/014G0801/device.js
@@ -1,30 +1,37 @@
 'use strict';
 
-const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+const { ZwaveDevice } = require('homey-meshdriver');
 
 class DevoloRadiatorThermostat extends ZwaveDevice {
 
-	onMeshInit() {
-		this.registerCapability('measure_battery', 'BATTERY');
-		this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT');
+  onMeshInit() {
+    this.registerCapability('measure_battery', 'BATTERY', {
+      getOpts: {
+        getOnOnline: true,
+      },
+    });
+    this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT', {
+      getOpts: {
+        getOnOnline: true,
+      },
+    });
 
-		// Since this node doesn't report the SENSOR_MULTILVEL commandclass but uses it anyway, parse it here.
-		this.node.on('unknownReport', reportBuffer => {
-			this.log('Unknowreport received', reportBuffer);
-			// TODO check the type byte in the reportBuffer
-			if (reportBuffer) {
-				try {
-					const sensorValue = (reportBuffer.readIntBE(4, 2)) / Math.pow(10, 2);
-					this.setCapabilityValue('measure_temperature', sensorValue);
-					this.log('Measured temperature', sensorValue);
-				} catch (error) {
-					this.error(error);
-				}
-			}
-		});
-	}
+    // Since this node doesn't report the SENSOR_MULTILVEL commandclass but uses it, parse it here.
+    this.node.on('unknownReport', reportBuffer => {
+      this.log('Unknowreport received', reportBuffer);
+      // TODO check the type byte in the reportBuffer
+      if (reportBuffer) {
+        try {
+          const sensorValue = (reportBuffer.readIntBE(4, 2)) / 100;
+          this.setCapabilityValue('measure_temperature', sensorValue);
+          this.log('Measured temperature', sensorValue);
+        } catch (error) {
+          this.error(error);
+        }
+      }
+    });
+  }
 
 }
-
 
 module.exports = DevoloRadiatorThermostat;
